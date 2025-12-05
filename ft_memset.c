4ab780a8e55d874f6e884274a7bbccdf4bdea001
Jens D <jdreissi@3-C-7.42heilbronn.de>
2025-12-05 14:17:57 +0100

    fixed include

--- ft_memset.c
+++ ft_memset.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#include "ft_printf.h"
 
 void	*ft_memset(void *s, int c, size_t n)
 {
